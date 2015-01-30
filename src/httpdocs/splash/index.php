fc897213e40dfc9b5306871807b967d9f6e14173
ejaxon <eric@deepweave.com>
2015-01-30 18:16:42 -0500

    testing

--- src/httpdocs/splash/index.php
+++ src/httpdocs/splash/index.php
@@ -52,7 +52,7 @@
 				</div>
 				<div class='header-right span6'>
 					<p>
-						a collaborative web effort brought to you by:
+						a collaboratively web effort brought to you by:
 					</p>									
 					<a href="http://www.codeforasheville.org" target="_blank">
 						Code for Asheville
