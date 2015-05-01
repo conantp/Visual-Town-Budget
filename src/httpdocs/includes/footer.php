c46f0f29158159b887a18531976ce562240e8a49
adamajm <adam.ajm@gmail.com>
2015-05-01 14:50:50 -0400

    updated footer to Raleigh txt

--- src/httpdocs/includes/footer.php
+++ src/httpdocs/includes/footer.php
@@ -1,6 +1,6 @@
  <div id="footer" style="font-size:1.2em">
-  <a class="link" target="_blank" href="http://www.ashevillenc.gov/">City of Asheville</a> - 
-  <a class="link" target="_blank" href="http://codeforasheville.org/">Developed by Code for Asheville</a> -
-  <a class="link" target="_blank" href="http://www.goinvo.com">Originally by Involution Studios</a> -
+  <a class="link" target="_blank" href="http://www.raleighnc.gov/">City of Raleigh</a> - 
+  <a class="link" target="_blank" href="http://codeforasheville.org/">Original by Code for Asheville</a> &
+  <a class="link" target="_blank" href="http://www.goinvo.com">Involution Studios</a> -
   <a class="link" href="/opendata"> Open Data + Code</a>
 </div>
